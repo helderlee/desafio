adee3f6f881cae25172ecc3e125f1696c6bcba6f
Helder Lee <Helder Lee@DESKTOP-GAIENTN.ti.crearj>
2018-04-04 10:18:30 -0300

    Desafio

--- desafio/src/main/java/pro/helderlee/desafio/repository/PlanetaRepository.java
+++ desafio/src/main/java/pro/helderlee/desafio/repository/PlanetaRepository.java
@@ -3,9 +3,11 @@ package pro.helderlee.desafio.repository;
 import java.math.BigInteger;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import pro.helderlee.desafio.modelo.Planeta;
 
+@Repository
 public interface PlanetaRepository extends MongoRepository<Planeta, BigInteger> {
 	
 	Planeta findByNome(String nome);
